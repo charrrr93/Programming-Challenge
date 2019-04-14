ca2db86f956dc909a86ae0b57a2a33f865034627
Charlotte Campbell <charlotte.campbell@team.telstra.com>
2019-04-14 20:20:04 +1000

    Put equations into a list, turned 6 functions into 1 function

--- crosspuzzle.py
+++ crosspuzzle.py
@@ -1,57 +1,19 @@
+tests = [['a', '+', 'b', '+', 'c', 15],
+         ['a', '+', 'd', '-', 'g', 3],
+         ['d', '+', 'e', '*', 'f', 24],
+         ['b', '*', 'e', '-', 'h', 12],
+         ['g', '+', 'h', '-', 'i', 14],
+         ['c', '/', 'f', '/', 'i', 4]]
 
-def test1(vars):
-    a = vars['a']
-    b = vars['b']
-    c = vars['c']
-    if (a + b) + c == 15:
+
+def test(vars, tests, number):
+    test = tests[number]
+    test_string = '(' + str(vars[test[0]]) + test[1] + str(vars[test[2]]) + ')' + test[3] + str(vars[test[4]])
+    if eval(test_string) == test[5]:
         return True
     else:
         return False
 
-def test2(vars):
-    a = vars['a']
-    d = vars['d']
-    g = vars['g']
-    if (a + d) - g == 3:
-        return True
-    else:
-        return False
-
-def test3(vars):
-    d = vars['d']
-    e = vars['e']
-    f = vars['f']
-    if (d + e) * f == 24:
-        return True
-    else:
-        return False
-
-def test4(vars):
-    b = vars['b']
-    e = vars['e']
-    h = vars['h']
-    if (b * e) - h == 12:
-        return True
-    else:
-        return False
-
-def test5(vars):
-    i = vars['i']
-    g = vars['g']
-    h = vars['h']
-    if g + h - i == 14:
-        return True
-    else:
-        return False
-
-def test6(vars):
-    c = vars['c']
-    f = vars['f']
-    i = vars['i']
-    if c / f / i == 4:
-        return True
-    else:
-        return False
 
 vars = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0}
 
@@ -63,29 +25,29 @@ for a in range(1,10):
             for c in range(1,10):
                 if c not in vars.values():
                     vars['c'] = c
-                    if test1(vars):
+                    if test(vars, tests, 0):
                         for d in range(1,10):
                             if d not in vars.values():
                                 vars['d'] = d
                                 for g in range(1,10):
                                     if g not in vars.values():
                                         vars['g'] = g
-                                        if test2(vars):
+                                        if test(vars, tests, 1):
                                             for e in range(1,10):
                                                 if e not in vars.values():
                                                     vars['e'] = e
                                                     for f in range(1,10):
                                                         if f not in vars.values():
                                                             vars['f'] = f
-                                                            if test3(vars):
+                                                            if test(vars, tests, 2):
                                                                 for h in range(1,10):
                                                                     if h not in vars.values():
                                                                         vars['h'] = h
-                                                                        if test4(vars):
+                                                                        if test(vars, tests, 3):
                                                                             for i in range(1,10):
                                                                                 if i not in vars.values():
                                                                                     vars['i'] = i
-                                                                                    if test5(vars) and test6(vars):
+                                                                                    if test(vars, tests, 4) and test(vars, tests, 5):
                                                                                         print(vars)
                                                                             vars['i'] = 0
                                                                 vars['h'] = 0
