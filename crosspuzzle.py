5e083c43b7c72c3bf81c73a68f43b6439aa121d5
Charlotte Campbell <charlotte.campbell@team.telstra.com>
2019-04-23 08:17:08 +1000

    Built GUI, need to redo test to incorporate GUI input + work on GUI input

--- crosspuzzle.py
+++ crosspuzzle.py
@@ -1,11 +1,5 @@
 import itertools
-
-tests = [['a', '+', 'b', '+', 'c', 15],
-         ['a', '+', 'd', '-', 'g', 3],
-         ['d', '+', 'e', '*', 'f', 24],
-         ['b', '*', 'e', '-', 'h', 12],
-         ['g', '+', 'h', '-', 'i', 14],
-         ['c', '/', 'f', '/', 'i', 4]]
+from tkinter import *
 
 
 def test(vars, tests, number):
@@ -17,9 +11,48 @@ def test(vars, tests, number):
         return False
 
 
+def clicked():
+    for entry in squareEntries:
+        squareVars.append(entry.get())
+    print(squareVars)
+
+
+tests = [['a', 'b', 'c'],
+         ['a', 'd', 'g'],
+         ['d', 'e', 'f'],
+         ['b', 'e', 'h'],
+         ['g', 'h', 'i'],
+         ['c', 'f', 'i']]
 count = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 vars = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0}
 
+squareEntries = []
+squareResults = []
+squareVars= []
+equalSquares = [5, 19, 33, 35, 37, 39]
+greySquares = [8, 10, 12, 13, 22, 24, 26, 27, 36, 38, 40, 41, 43, 45, 47, 48]
+colouredSquares = [1, 3, 6, 7, 9, 11, 15, 17, 20, 21, 23, 25, 29, 31, 34, 42, 44, 46]
+window = Tk()
+window.title("Cross Maths Puzzle")
+squareCount = 0
+for i in range(1, 8):
+    for j in range(1, 8):
+        if squareCount in equalSquares:
+            Label(window, text="=", font=("Arial Bold", 50), width=1, justify=CENTER).grid(column=i, row=j) 
+        elif squareCount in greySquares:
+            Entry(window, font=("Arial Bold", 50), width=2, state='disabled').grid(column=i, row=j)
+        elif squareCount in colouredSquares:
+            entry = Entry(window, font=("Arial Bold", 50), width=2, justify=CENTER)
+            squareEntries.append(entry)
+            entry.grid(column=i, row=j)
+        else:
+            entry = Entry(window, font=("Arial Bold", 50), width=2, bg="old lace", justify=CENTER)
+            squareResults.append(entry)
+            entry.grid(column=i, row=j)
+        squareCount += 1
+button = Button(window, text="Solve Puzzle", command=clicked, font=("Arial Bold", 30), justify=CENTER)
+button.grid(column=1, row=8, columnspan=7)
+mainloop()
 
 for item in list(itertools.product(count, count, count)):
     if len(item) == len(dict.fromkeys(item)):
